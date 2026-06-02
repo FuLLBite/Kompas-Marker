401aa994af1d7f390841ddfcdecd620732ff0dbc
Dornes <gleacsun@gmail.com>
2026-06-02 15:03:15 +0300

    Add KeyPress

--- MainApp/MainApp.py
+++ MainApp/MainApp.py
@@ -2,6 +2,11 @@ import Marker
 from tkinter import *
 from tkinter import ttk
 
+# Константы
+UP = 90
+DOWN = -90
+LEFT = 180
+RIGHT = 0
 
 def GetLineParam(order, direct, profile=None ,name=None):
     # Получение параметров о наименовании и направлении ссылки сигнала
@@ -20,15 +25,20 @@ def GetLineParam(order, direct, profile=None ,name=None):
 def ChooseMenuF():
     # Получение получение информации о направлении ссылки
     # и наименование сигнала
+    def windAct(angle):
+        GetLineParam(1, angle, EntryProfile, EntryName)
+        Marker.getinfo1(1, app)
+        ChooseMenuS()
+        window1.destroy()
 
     # Настройка окна
-    window1 = Tk()
+    window1 = Toplevel(app.root)
     window1.title("Первая точка")
     window1.geometry("250x300")
     window1.attributes('-topmost', True)
 
     # текстовая метка
-    label1 = ttk.Label(window1, text="Наименование сигнала")
+    label1 = ttk.Label(window1, text="Наименование сигнала", background='#f0f0f0')
     label1.pack(anchor='n', padx=20, pady=5)
 
     # Вывод в окно поля для получения наименования сигнала
@@ -36,14 +46,14 @@ def ChooseMenuF():
     EntryName.pack(anchor='n', padx=20, pady=5)
 
     # текстовая метка
-    label12 = ttk.Label(window1, text="Сечение провода в мм^2")
+    label12 = ttk.Label(window1, text="Сечение провода в мм^2", background='#f0f0f0')
     label12.pack(anchor='n', padx=20, pady=5)
 
     # Вывод в окно поля для получения сечения проводника
     EntryProfile = ttk.Entry(window1)
     EntryProfile.pack(anchor='n', padx=20, pady=5)
     # текстовая метка
-    label13 = ttk.Label(window1, text="Выберете направление ссылки")
+    label13 = ttk.Label(window1, text="Выберете направление ссылки", background='#f0f0f0')
     label13.pack(anchor='n', padx=20, pady=5)
     # Параметры кнопок
 
@@ -52,83 +62,82 @@ def ChooseMenuF():
     # Вывод в окно функциональных кнопок
     # Направление ссылки вверх
     directionUp = Button(window1, text=chr(11205), font=('', 15), width=WidthOfButton,
-                             command=lambda: [GetLineParam(1, 90, EntryProfile, EntryName),
-                                              Marker.getinfo1(1, app), ChooseMenuS(),
-                                              window1.destroy()])
+                             command=lambda: windAct(UP))
     directionUp.pack(padx=6, pady=6, anchor='c')
+    window1.bind('<Up>', lambda event: windAct(UP))
+    app.root.bind('<Up>', lambda event: windAct(UP))
     # Направление ссылки вниз
     directionDown = Button(window1, text=chr(11206), font=('', 15), width=WidthOfButton,
-                               command=lambda: [GetLineParam(1, -90, EntryProfile, EntryName),
-                                                Marker.getinfo1(1, app), ChooseMenuS(),
-                                                window1.destroy()])
+                               command=lambda: windAct(DOWN))
     directionDown.place(relx=0.41,rely=0.8)
+    window1.bind('<Down>', lambda event: windAct(DOWN))
+    app.root.bind('<Down>', lambda event: windAct(DOWN))
     # Направление ссылки вправо
     directionRight = Button(window1, text=chr(11208), font=('', 15), width=WidthOfButton,
-                                command=lambda: [GetLineParam(1, 0, EntryProfile, EntryName),
-                                                 Marker.getinfo1(1, app), ChooseMenuS(),
-                                                 window1.destroy()])
+                                command=lambda: windAct(RIGHT))
     directionRight.place(relx=0.6,rely=0.66)
+    window1.bind('<Right>', lambda event: windAct(RIGHT))
+    app.root.bind('<Right>', lambda event: windAct(RIGHT))
     # Направление ссылки влево
     directionLeft = Button(window1, text=chr(11207), font=('', 15), width=WidthOfButton,
-                               command=lambda: [GetLineParam(1, 180, EntryProfile, EntryName),
-                                                Marker.getinfo1(1, app),
-                                                ChooseMenuS(),
-                                                window1.destroy()])
+                               command=lambda: windAct(LEFT))
     directionLeft.place(relx=0.23,rely=0.66)
-
+    window1.bind('<Left>', lambda event: windAct(LEFT))
+    app.root.bind('<Left>', lambda event: windAct(LEFT))
 
 
 def ChooseMenuS():
     # Получение получение информации о направлении ссылки
     # и наименование сигнала
 
+    def windAct(angle):
+        GetLineParam(2, angle ),
+        Marker.getinfo1(2, app),
+        Marker.mark(app.Fx, app.Fy, app.DirectionF, app.Sx,
+                    app.Sy, app.LineName, app.DirectionS,
+                    app.Profile),
+        window2.destroy()
+
     # Настройка окна
-    window2 = Tk()
+
+    window2 = Toplevel(app.root)
     window2.title("Вторая точка")
-    window2.geometry("250x200")
+    window2.geometry("280x200")
     window2.attributes('-topmost', True)
+    #window2.focus_force()
     # Параметры кнопок
-    position = {"padx":6, "pady":6, "anchor":"c"}
+
     WidthOfButton = 3
-    label13 = ttk.Label(window2, text="Выберете направление второй ссылки")
+    label13 = ttk.Label(window2, text="Выберете направление второй ссылки", background='#f0f0f0')
     label13.pack(anchor='n', padx=20, pady=5)
+
+
     # Вывод в окно функциональных кнопок
     # Направление ссылки вверх
+
     directionUp = Button(window2, text=chr(11205), font=('', 15), width=WidthOfButton,
-                             command=lambda: [GetLineParam( 2, 90, ),
-                                             Marker.getinfo1(2, app),
-                                             Marker.mark(app.Fx, app.Fy, app.DirectionF, app.Sx,
-                                                         app.Sy, app.LineName, app.DirectionS,
-                                                         app.Profile),
-                                             window2.destroy()])
+                             command=lambda: windAct(UP))
     directionUp.pack(padx=6, pady=6, anchor='c')
+    window2.bind('<Up>', lambda event: windAct(UP))
+    app.root.bind('<Up>', lambda event: windAct(UP))
     # Направление ссылки вниз
     directionDown = Button(window2, text=chr(11206), font=('', 15), width=WidthOfButton,
-                               command=lambda: [GetLineParam(2, -90),
-                                                Marker.getinfo1(2, app),
-                                                Marker.mark(app.Fx, app.Fy, app.DirectionF, app.Sx,
-                                                            app.Sy, app.LineName, app.DirectionS,
-                                                            app.Profile),
-                                                window2.destroy()])
+                               command=lambda: windAct(DOWN))
     directionDown.place(relx=0.41,rely=0.62)
+    window2.bind('<Down>', lambda event: windAct(DOWN))
+    app.root.bind('<Down>', lambda event: windAct(DOWN))
     # Направление ссылки вправо
     directionRight = Button(window2, text=chr(11208), font=('', 15), width=WidthOfButton,
-                                command=lambda: [GetLineParam(2, 0),
-                                                 Marker.getinfo1(2, app),
-                                                 Marker.mark(app.Fx, app.Fy, app.DirectionF, app.Sx,
-                                                             app.Sy, app.LineName, app.DirectionS,
-                                                             app.Profile),
-                                                 window2.destroy()])
+                                command=lambda: windAct(RIGHT))
     directionRight.place(relx=0.6,rely=0.4)
+    window2.bind('<Right>', lambda event: windAct(RIGHT))
+    app.root.bind('<Right>', lambda event: windAct(RIGHT))
     # Направление ссылки влево
     directionLeft = Button(window2, text=chr(11207), font=('', 15), width=WidthOfButton,
-                               command=lambda: [GetLineParam( 2, 180),
-                                                Marker.getinfo1(2, app),
-                                                Marker.mark(app.Fx, app.Fy, app.DirectionF, app.Sx,
-                                                            app.Sy, app.LineName, app.DirectionS,
-                                                            app.Profile),
-                                                window2.destroy()])
+                               command=lambda: windAct(LEFT))
     directionLeft.place(relx=0.23,rely=0.4)
+    window2.bind('<Left>', lambda event: windAct(LEFT))
+    app.root.bind('<Left>', lambda event: windAct(LEFT))
 
 
 
@@ -161,6 +170,7 @@ class App:
         # Добавление кнопки пользователя
         btn = ttk.Button(self.root, text="Добавить ссылку", command=ChooseMenuF)
         btn.pack(expand=True)
+        self.root.bind('<Return>', lambda event: ChooseMenuF())
     def run(self):
         self.root.mainloop()
 
@@ -168,8 +178,6 @@ class App:
 app = App()
 app.run()
 
-# Вместо надписей на кнопках сделать стрелочки
-# Сделать окна выбора классами, а не функциями
 # Сделать меню с настройками и справкой
 # Добавить ввод направлений с клавиатуры
 
--- MainApp/Test.py
+++ MainApp/Test.py
@@ -1,30 +1,35 @@
-try:  # Python-2
-    from Tkinter import *
-    import tkFont
-except ImportError:  # Python-3
-    from tkinter import *
-    from tkinter import font as tkFont
-# using grid
-# +------+-------------+
-# | btn1 |    btn2     |
-# +------+------+------+
-# | btn3 | btn3 | btn4 |
-# +-------------+------+
-root = Tk()
+import Marker
+from tkinter import *
+from tkinter import ttk
 
-# tkFont.BOLD == 'bold'
-helv36 = tkFont.Font(family='Helvetica', size=36, weight=tkFont.BOLD)
-btn1 = Button(text='btn1', font=helv36)
-btn2 = Button(text='btn2', font=helv36)
-btn3 = Button(text='btn3', font=helv36)
-btn4 = Button(text='btn4', font=helv36)
-btn5 = Button(text='btn5', font=helv36)
-root.rowconfigure((0,1), weight=1)  # make buttons stretch when
-root.columnconfigure((0,2), weight=1)  # when window is resized
-btn1.grid(row=0, column=0, columnspan=1, sticky='EWNS')
-btn2.grid(row=0, column=1, columnspan=2, sticky='EWNS')
-btn3.grid(row=1, column=0, columnspan=1, sticky='EWNS')
-btn4.grid(row=1, column=1, columnspan=1, sticky='EWNS')
-btn5.grid(row=1, column=2, columnspan=1, sticky='EWNS')
+def window1():
+    window1 = Tk()
+    window1.title("Первая точка")
+    window1.geometry("250x300")
+    window1.attributes('-topmost', True)
+    directionUp = Button(window1, text="Закрыть",
+                         command=window1.destroy)
+    directionUp.pack(padx=6, pady=6, anchor='c')
 
-root.mainloop()
+class App:
+
+    def __init__(self):
+        # Настройка окна
+        self.root = Tk()
+        ttk.Style().theme_use('clam')
+        self.root.title("Kompas Маркер")
+        self.root.geometry("300x50")
+        self.root.attributes('-topmost', True)
+
+
+        # Добавление кнопки пользователя
+        btn = ttk.Button(self.root, text="Добавить ссылку", command=window1)
+        btn.pack(expand=True)
+        self.root.bind('<Up>', lambda event: window1())
+    def run(self):
+        self.root.mainloop()
+#command=ChooseMenuF)
+
+
+app = App()
+app.run()
